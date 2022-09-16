1290403d012de9b601378d5f57b16e21ad33dfae
Farah Moataz <farahmoataz90@gmail.com>
2022-09-16 15:57:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// start of loading page 
+var loader=document.getElementById("preloader");
+window.addEventListener("load" ,function()  //after loading remove it
+{
+  loader.style.display="none";
+})
+// end of loading page 
+
